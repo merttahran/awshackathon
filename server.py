6d071b048ab4082d49993da7e3b9f75e215266e0
Mert Tahran <44242500+merttahran@users.noreply.github.com>
2019-09-21 10:44:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ('34.240.94.33', 80)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
